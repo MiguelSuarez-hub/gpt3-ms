65f5bc28a5d4cdab1a094d277556ef773929ca25
Mike <miguel.suarezf05@gmail.com>
2023-05-06 08:18:07 -0500

    Added structure and styles to footer component

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
+
+const linksData = [
+  {
+    title:'Links',
+    links:['Overons', 'Social Media', 'Counters', 'Contact' ]
+  },
+  {
+    title:'Company',
+    links:['Terms & Conditions', 'Privacy Policy', 'Contact' ]
+  },
+  {
+    title:'Get in touch',
+    links:['Crechterwoord K12 182 DK Alknjkcb', '085-132567', 'info@payme.net' ]
+  }
+];
+
+const LinkColumn = ({ title, links }) => (
+  <div className='gpt3__footer-links_column'>
+    <h4>{title}</h4>
+    {links.map((item, index)=> <a href='#' key={item + index}>{item}</a>
+    )};
+  </div>
+)
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>       
+      </div>
+      <div className='gpt3__footer-button'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        {linksData.map((item, index) =>
+        <LinkColumn title={item.title} links={item.links} key={item.title + index}/>
+        )}     
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
