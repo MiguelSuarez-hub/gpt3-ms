f28a202a7ae52aea387a4d89ca145c7816d7cd9a
Mike <miguel.suarezf05@gmail.com>
2023-05-05 23:33:13 -0500

    Added structure and styles for article and blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 
 import './blog.css';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
+
+const articlesData = [
+  {
+    imgUrl: blog02,
+    date: 'Sep 26, 2021',
+    text: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'
+  },
+  {
+    imgUrl: blog03,
+    date: 'Sep 26, 2021',
+    text: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'
+  },
+  {
+    imgUrl: blog04,
+    date: 'Sep 26, 2021',
+    text: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'
+  },
+  {
+    imgUrl: blog05,
+    date: 'Sep 26, 2021',
+    text: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'
+  }
+];
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening,<br/> We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date='Sep 26, 2021' text='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          {articlesData.map(( item, index) => <Article imgUrl={item.imgUrl} date={item.date} text={item.text} key={item.text + index}/>)}
+        </div>
+      </div>
+    </div>
   )
 }
 
